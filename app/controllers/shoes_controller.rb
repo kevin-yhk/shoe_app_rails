e23d7594b233e99939826868d827de76be55c3a6
kevinkim <kevinkim.yhk@gmail.com>
2020-05-10 13:46:50 -0700

    added in new and create method in addition to private method params for refactoring

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -1,2 +1,21 @@
 class ShoesController < ApplicationController
+
+    def new 
+        @shoe = Shoe.new
+    end
+
+    def create
+        @shoe = Shoe.create(shoe_params)
+        if @shoe.valid?
+            redirect_to @shoe
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def shoe_params
+        params.require(:shoe).permit(:model, :brand)
+    end
 end
