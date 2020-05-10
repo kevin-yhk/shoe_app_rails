8a8e757bf3a99b1eb9935cd3a1f2c85f8e121965
kevinkim <kevinkim.yhk@gmail.com>
2020-05-10 12:38:30 -0700

    created new and create methods for a user signing in, in addition to a user signing out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,20 @@
 class SessionsController < ApplicationController
+    def new
+        render :new
+    end
+
+    def create
+        @user = User.find_by(:email => params[:email])
+        if @user && @user.authenticate(params[:password])
+          session[:user_id] = @user.id
+          redirect_to user_path(@user)
+        else
+          render :new
+        end
+      end
+
+    def destroy
+          session.delete :user_id
+          redirect_to root_url
+    end
 end
