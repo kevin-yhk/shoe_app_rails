61a7f73a3cae2620eb78225b18fcab83c358858f
kevinkim <kevinkim.yhk@gmail.com>
2020-05-10 12:18:57 -0700

    added in new and create methods for the creation of new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,18 @@
 class UsersController < ApplicationController
-    
-    
-    
+    def new 
+        @user = User.new
+    end
+
+    def create
+        @user = User.create(user_params)
+        if @user.valid? 
+            session[:user_id] = @user.id
+            redirect_to user_path
+        else
+            render :new
+        end
+    end
+
     private
 
     def user_params
