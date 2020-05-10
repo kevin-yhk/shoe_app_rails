a70c2a8bd8d4587c1428f999875b33381773d6a0
kevinkim <kevinkim.yhk@gmail.com>
2020-05-10 12:15:45 -0700

    added in private method for user_params for refactoring purposes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,10 @@
 class UsersController < ApplicationController
-   
+    
+    
+    
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password, :email)
+    end 
 end
