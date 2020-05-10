f985b5a6f55e4211cf07a785c122410c094fc78b
kevinkim <kevinkim.yhk@gmail.com>
2020-05-10 12:05:03 -0700

    added in routes to user interface for logging in, signing up, and logging out

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  
+  
   resources :reviews
   resources :shoes
+
+  get '/signup', to:'users#new'
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
