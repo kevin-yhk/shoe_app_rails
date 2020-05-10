b381075361cefd6247d07145c94e779b1b35f358
kevinkim <kevinkim.yhk@gmail.com>
2020-05-10 14:03:20 -0700

    fixed typo error

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -24,7 +24,7 @@ class ReviewsController < ApplicationController
     private
     
     def review_params
-        params.require(:review).permit(:rating, :review, :user_id)
+        params.require(:review).permit(:title, :review, :user_id)
     end
 
     def current_shoe
