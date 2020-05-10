cc5e3442bfc5410206b101d3a860609a59973657
kevinkim <kevinkim.yhk@gmail.com>
2020-05-10 13:33:32 -0700

    added new and create method for reviews in relation to shoes and user

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,19 @@
 class ReviewsController < ApplicationController
+
+    def new 
+        if params[:shoe_id]
+            @shoe = Shoe.find_by(id: params[:shoe_id])
+            @review = @shoe.reviews.new(user_id: @user.id)
+        end
+    end
+
+    def create
+        @review = @shoe.reviews.create(review_params)
+        if @review.valid? 
+            @shoe = @review.shoe
+            redirect_to shoe_review_path(@shoe, @review)
+        else
+            render :new
+        end
+    end
 end
