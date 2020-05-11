484336dd5f871eff522ebe79119c4e3cc1affc4b
kevinkim <kevinkim.yhk@gmail.com>
2020-05-10 19:42:21 -0700

    rewrote facebook authentication to make it work again due to lost connection of routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  
+
   get '/auth/facebook/callback' => 'sessions#facebook'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
