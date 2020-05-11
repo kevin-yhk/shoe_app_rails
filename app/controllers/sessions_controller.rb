d561ff449fe6f2101246273eb3f1f7b801d6eef4
kevinkim <kevinkim.yhk@gmail.com>
2020-05-10 19:33:46 -0700

    created facebook omniauth capabilities

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,8 +13,23 @@ class SessionsController < ApplicationController
         end
       end
 
+      def facebook
+        @user = User.find_or_create_by(email: auth['info']['email']) do |u|
+          u.username = auth['info']['name']
+          u.password = SecureRandom.hex
+          end
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+      end  
+
     def destroy
           session.delete :user_id
           redirect_to root_url
     end
+
+    private
+ 
+	def auth
+	  request.env['omniauth.auth']
+	end
 end
